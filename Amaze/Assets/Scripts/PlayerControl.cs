e70918cd8edd63567017e13f1fbef8ef9790a20c
Learner062022 <ddylan246@yahoo.com>
2025-05-15 15:42:10 +0800

    feat(player, scene): improve rotation, scene organization, and project settings
    - Reorganized scene structure by relocating player and plane from MainScene to PlayerScene.
    - Updated GameScene and MainMenuScene to reflect new hierarchy.
    - Locked cursor to center it within the play area for better aiming.
    - Enhanced PlayerControl script with headers for readability.
    - Added public transform for camera rotation and aiming functionality.
    - Adjusted sensitivity for X and Y axis rotation.
    - Set min/max rotation limits to prevent camera errors.
    - Invoked Look function inside Update() for continuous rotation.
    - Retrieved mouse movement data for aiming functionality.
    - Updated player prefab to align with new scene structure.
    - Modified EditorBuildSettings to accommodate scene changes.

--- Amaze/Assets/Scripts/PlayerControl.cs
+++ Amaze/Assets/Scripts/PlayerControl.cs
@@ -4,24 +4,43 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    [Header("Movement")]
     public float moveSpeed = 8f;
     public float gravity = -20f;
     CharacterController characterController;
     Vector3 inputVector;
 
-    void Start() =>
-        characterController = GetComponent<CharacterController>();
 
+    [Header("Looking")]
+    public Transform lookCamera;
+    public float sensitivityX = 15f;
+    public float sensitivityY = 15f;
+
+    public float minY = -90;
+    public float maxY = 90;
+
+    float currentYRotation;
+    Vector2 aimVector;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+        
     void Update()
     {
         GetInput();
         Move();
+        Look();
     }
 
     void GetInput()
     {
         inputVector.x = Input.GetAxis("Horizontal");
         inputVector.y = Input.GetAxis("Vertical");
+        aimVector.x = Input.GetAxis("Mouse X");
+        aimVector.y = Input.GetAxis("Mouse Y");
     }
 
     void Move()
@@ -31,5 +50,13 @@ public class PlayerControl : MonoBehaviour
         moveVector.y = gravity;
         moveVector *= Time.deltaTime;
         characterController.Move(moveVector);
+    }
+
+    void Look()
+    {
+        transform.Rotate(transform.up, aimVector.x * sensitivityX);
+        currentYRotation += aimVector.y * sensitivityY;
+        currentYRotation = Mathf.Clamp(currentYRotation, minY, maxY);
+        lookCamera.eulerAngles = new Vector3(-currentYRotation, lookCamera.eulerAngles.y, lookCamera.eulerAngles.z);
     }
 }
