459b0f50ee66eabcb5564acf8df5645b12b1fa75
Learner062022 <ddylan246@yahoo.com>
2025-07-31 11:52:10 +0800

    feat(maze-cell): define cell type and visited state for maze cells

---
+++ Amaze/Assets/Scripts/MazeCell.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts
+{
+    /// <summary>
+    /// Represents a cell in a maze, including its type and visited state.
+    /// </summary>
+    public class MazeCell
+    {
+        enum CellType { 
+            Wall,
+            Path,
+            Start,
+            End
+        }
+
+        bool isVisited; 
+    }
+}
