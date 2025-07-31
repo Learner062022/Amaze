91a36aae247f932f60f45ca3a39015dc4eb1f9b5
Learner062022 <ddylan246@yahoo.com>
2025-07-31 10:25:32 +0800

    feat(maze-generator): implement maze generation using MazeConfig and selected algorithm

---
+++ Amaze/Assets/Scripts/MazeGenerator.cs
@@ -0,0 +1,24 @@
+using Assets.Scripts;
+using PlasticPipe.PlasticProtocol.Messages;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Generates the maze using parameters from MazeConfig.
+/// Responsible for applying the selected algorithm and building the maze structure.
+/// </summary>
+public class MazeGenerator : MonoBehaviour
+{
+    MazeConfig config;
+
+    void Start() {
+        // Access MazeConfig for generation parameters
+        // TODO: Use config to generate maze based on selected difficulty
+    }
+
+    void Update() {
+        // Handle runtime updates and interactions
+    }
+}
