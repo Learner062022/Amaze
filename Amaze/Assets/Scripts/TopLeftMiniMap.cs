6d94d413da9f370532d95ce3e3c34a0642387d90
Learner062022 <ddylan246@yahoo.com>
2025-07-31 09:28:27 +0800

    feat(top-left-minmimap): create class inheriting from minimap to track the player's position within the sub-mazes residing in the outer maze's cells

---
+++ Amaze/Assets/Scripts/TopLeftMiniMap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts
+{
+    /// <summary>
+    /// Tracks the player's position within the sub-mazes that reside in the cells of the outer maze.
+    /// </summary>
+    public class TopLeftMiniMap : MiniMap
+    {
+
+    }
+}
