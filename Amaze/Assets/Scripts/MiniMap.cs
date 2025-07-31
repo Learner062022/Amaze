02206cfc9391d77c9771782886ab2ff59de07e13
Learner062022 <ddylan246@yahoo.com>
2025-07-31 09:18:34 +0800

    feat(minimap): create base class to manage real-time player positioning in the minimap UI.

---
+++ Amaze/Assets/Scripts/MiniMap.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    /// <summary>
+    /// Base class for minimap functionality.
+    /// Handles positioning and tracking of player location within the minimap UI.
+    /// Intended to be extended by specific minimap layout implementations (e.g., TopLeftMiniMap, BottomRightMiniMap)
+    /// </summary>
+    public class MiniMap : MonoBehaviour
+    {
+        [SerializeField] 
+        RectTransform miniMapRectTransform;
+
+        [SerializeField]
+        Transform playersLocation;
+    }
+}
