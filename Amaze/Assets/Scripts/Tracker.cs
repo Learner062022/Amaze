d81207227320bbd00ee57df79d5dc4124a17377c
Learner062022 <ddylan246@yahoo.com>
2025-08-09 13:39:25 +0800

    feat(tracker): track entities' positions within the maze

---
+++ Amaze/Assets/Scripts/Tracker.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    /// <summary>
+    /// Tracks the positions of entities within the maze.
+    /// </summary>
+    public class Tracker : MonoBehaviour
+    {
+        [SerializeField]
+        GameObject player;
+
+        [SerializeField]
+        GameObject collectable;
+
+        Vector3 playerPos;
+        Vector3 collectablePos;
+
+        void Start()
+        {
+            playerPos = player.transform.position;
+            collectablePos = collectable.transform.position;
+        }
+
+        void Update()
+        {
+            if (player.transform.position != playerPos)
+            {
+                playerPos = player.transform.position;
+                if (playerPos == collectablePos)
+                {
+                    Destroy(collectable);
+                    // add collectable to HUD
+                }
+            }
+        }
+    }
+}
