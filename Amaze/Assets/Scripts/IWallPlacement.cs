b0a59dca6873805cd9cf59b987ed0b4b4d6923b8
Learner062022 <ddylan246@yahoo.com>
2025-08-11 14:01:27 +0800

    feat(wall-placement): add wall placement interface

---
+++ Amaze/Assets/Scripts/IWallPlacement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts
+{
+    /// <summary>
+    /// Calculates the world position and rotation for walls between adjacent cells and along maze edges.
+    /// </summary>
+    public interface IWallPlacement
+    {
+        
+    }
+}
