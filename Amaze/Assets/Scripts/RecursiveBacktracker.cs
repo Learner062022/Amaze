75c5b82a0379665015efff1c76095f8734d1d687
Learner062022 <ddylan246@yahoo.com>
2025-08-11 16:27:21 +0800

    feat(algorithm): scaffold RecursiveBacktracker with summary content

---
+++ Amaze/Assets/Scripts/RecursiveBacktracker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts
+{
+    /// <summary>
+    /// Implements the Recursive Backtracker maze generation algorithm.
+    /// </summary>
+    public class RecursiveBacktracker : IAlgorithm
+    {
+
+    }
+}
