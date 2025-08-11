bf1a99cf18418ce53908d8feaf30611566629d94
Learner062022 <ddylan246@yahoo.com>
2025-08-11 15:22:45 +0800

    feat(renderer): scaffold MazeRenderer with TODOs for wall placement and rendering

---
+++ Amaze/Assets/Scripts/MazeRenderer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    /// <summary>
+    /// Handles visual instantiation of maze elements based on grid data.
+    /// </summary>
+    public class MazeRenderer
+    {
+        GameObject mazeRoot;
+
+        // TODO: Define and assign wall prefab GameObjects for rendering
+        // TODO: Initialize mazeRoot GameObject to parent all maze elements
+        // TODO: Instantiate wall prefabs based on MazeCell wall flags
+        // TODO: Calculate each cell's world position from array index, maze origin, and tile size
+        // TODO: Offset cell positions correctly in world space
+        // TODO: Share wall references between adjacent cells 
+        // TODO: Handle vertical wall offsets 
+        // TODO: Parent all instantiated elements under mazeRoot
+    }
+}
