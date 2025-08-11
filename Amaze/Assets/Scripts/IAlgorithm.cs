cd39e16e5ab9850ad8ec72701241442ff3e2063f
Learner062022 <ddylan246@yahoo.com>
2025-08-11 11:08:37 +0800

    feat(generation): add interface to allow interchangeable maze generation algorithms

---
+++ Amaze/Assets/Scripts/IAlgorithm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts
+{
+    /// <summary>
+    /// Interface allowing different maze generation algorithms.
+    /// </summary>
+    public interface IAlgorithm
+    {
+
+    }
+}
