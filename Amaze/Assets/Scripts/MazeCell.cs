55c0d2e993e4d0dd25fdd2bbd9efb33e976d9d6e
Learner062022 <ddylan246@yahoo.com>
2025-08-11 12:56:03 +0800

    refactor(maze-cell): remove the CellType Enum and place it with properties indicating wall presence in each direction
    Cell type and wall placement are inferred from wall flags, visited state and visitation order; wall presence is directionally defined and determined by the agent's movement, neighbor existence and visitation order.

--- Amaze/Assets/Scripts/MazeCell.cs
+++ Amaze/Assets/Scripts/MazeCell.cs
@@ -3,21 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Assets.Scripts
 {
     /// <summary>
-    /// Represents a cell in a maze, including its type and visited state.
+    /// Represents a cell in a maze, including its wall flags and visited state.
     /// </summary>
     public class MazeCell
     {
-        enum CellType { 
-            Wall,
-            Path,
-            Start,
-            End
-        }
-
-        bool isVisited; 
+        public bool HasNorthWall { get; set; } = true;
+        public bool HasEastWall { get; set; } = true;
+        public bool HasSouthWall { get; set; } = true;
+        public bool HasWestWall { get; set; } = true;
+        public bool IsVisited { get; set; } = false;
     }
 }
